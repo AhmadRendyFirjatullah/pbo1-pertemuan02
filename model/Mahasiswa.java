db6f25cbb5c08d9a1e4bc968b395e5e7eb12dc80
AhmadRendyFirjatullah <a.rendyfirjatullah@gmail.com>
2023-05-11 14:30:00 +0800

    Memanggil method dari banyak Class di pakcage lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println(); 
+  System.out.println("== Data Dosen ==================");
+  System.out.println("NPM  : 2110010084");
+  System.out.println("Nama : Ahmad Rendy Firjatullah");
+  System.out.println("================================");
+ }
+}
+  
